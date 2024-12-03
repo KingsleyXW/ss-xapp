8ac60d50aaf533829e739660d2cebdf99a48101b
Kshitij MSI <dkshitij29@gmail.com>
2024-12-03 01:04:26 -0500

    added the files related to throttle, crash report and cleanup script

--- zmqtwoue.sh
+++ zmqtwoue.sh
@@ -84,3 +84,108 @@ echo "Getting Slice (name=fast):" ; echo
 curl -i -X GET http://${SS_XAPP}:8000/v1/slices/fast; echo ; echo
 
 #creation of nodeb, ues, slices and binding
+
+
+
+#added from local machine 
+
+#changing the value to make sure the ue has time to move from one slice to another
+#SLEEPINT=10; the base station crashed
+SLEEPINT=7.5;
+#SLEEPINT=8;
+
+# so with 10 it is too late and with 5 it is too fast 
+
+# # # testing script to check the throotling part 
+
+while true; do
+    echo "Enter a command: (t - throttle, r - unthrottle, exit - to stop)"
+    read INPUT
+
+    case $INPUT in
+        "t")
+            echo "Throttling UE (imsi=001010123456789) by moving it to secure_slice:" ; echo
+            curl -i -X DELETE http://${SS_XAPP}:8000/v1/slices/fast/ues/001010123456789 ; echo ; echo
+            sleep $SLEEPINT
+            curl -i -X POST http://${SS_XAPP}:8000/v1/slices/secure_slice/ues/001010123456789 ; echo ; echo
+            ;;
+
+        "r")    
+            echo "Unthrottling UE (imsi=001010123456789) by moving it back to fast slice:" ; echo
+            curl -i -X DELETE http://${SS_XAPP}:8000/v1/slices/secure_slice/ues/001010123456789 ; echo ; echo
+            sleep $SLEEPINT
+            curl -i -X POST http://${SS_XAPP}:8000/v1/slices/fast/ues/001010123456789 ; echo ; echo
+            ;;
+
+        "exit")
+            echo "Exiting script..."
+            break
+            ;;
+
+        *)
+            echo "Invalid command. Please enter 't' to throttle, 'rr' to unthrottle, or 'exit' to stop."
+            ;;
+    esac
+
+    
+    sleep $SLEEPINT
+done
+
+
+# while true; do
+#     echo "Enter a command: (t - throttle, r - unthrottle, exit - to stop)"
+#     read INPUT
+
+#     case $INPUT in
+#         "t")
+#             echo "Throttling UE (imsi=001010123456789) by moving it to secure_slice:" ; echo
+#             # Removing UE from fast slice
+#             RESPONSE=$(curl -i -s -o response.txt -w "%{http_code}" -X DELETE http://${SS_XAPP}:8000/v1/slices/fast/ues/001010123456789)
+#             if [ "$RESPONSE" != "200" ]; then
+#                 echo "Failed to remove UE from fast slice. Status Code: $RESPONSE"
+#                 cat response.txt
+#                 exit 1
+#             fi
+#             sleep $SLEEPINT
+#             # Adding UE to secure_slice
+#             RESPONSE=$(curl -i -s -o response.txt -w "%{http_code}" -X POST http://${SS_XAPP}:8000/v1/slices/secure_slice/ues/001010123456789)
+#             if [ "$RESPONSE" != "200" ]; then
+#                 echo "Failed to add UE to secure_slice. Status Code: $RESPONSE"
+#                 cat response.txt
+#                 exit 1
+#             fi
+#             echo "Successfully throttled UE."
+#             ;;
+
+#         "r")    
+#             echo "Unthrottling UE (imsi=001010123456789) by moving it back to fast slice:" ; echo
+#             # Removing UE from secure_slice
+#             RESPONSE=$(curl -i -s -o response.txt -w "%{http_code}" -X DELETE http://${SS_XAPP}:8000/v1/slices/secure_slice/ues/001010123456789)
+#             if [ "$RESPONSE" != "200" ]; then
+#                 echo "Failed to remove UE from secure_slice. Status Code: $RESPONSE"
+#                 cat response.txt
+#                 exit 1
+#             fi
+#             sleep $SLEEPINT
+#             # Adding UE back to fast slice
+#             RESPONSE=$(curl -i -s -o response.txt -w "%{http_code}" -X POST http://${SS_XAPP}:8000/v1/slices/fast/ues/001010123456789)
+#             if [ "$RESPONSE" != "200" ]; then
+#                 echo "Failed to add UE back to fast slice. Status Code: $RESPONSE"
+#                 cat response.txt
+#                 exit 1
+#             fi
+#             echo "Successfully unthrottled UE."
+#             ;;
+
+#         "exit")
+#             echo "Exiting script..."
+#             break
+#             ;;
+
+#         *)
+#             echo "Invalid command. Please enter 't' to throttle, 'r' to unthrottle, or 'exit' to stop."
+#             ;;
+#     esac
+
+#     sleep $SLEEPINT
+# done
