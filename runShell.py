0696dad59475133058d0cca6d4eaee91450d6214
jawadhazime <139500736+jawadhazime@users.noreply.github.com>
2024-12-09 00:13:20 -0500

    Updated with new shell script

--- runShell.py
+++ runShell.py
@@ -1,7 +1,7 @@
 import subprocess
 
 #Small test script for running shell scripts
-cmd1 = ['sudo','./zmqtwoue.sh']
+cmd1 = ['sudo','./zmqtwoue_with_2slices.sh']
 shellscript = subprocess.Popen(cmd1, stdout=subprocess.PIPE,stderr=subprocess.STDOUT,stdin=subprocess.PIPE)
 stdout, stderr = shellscript.communicate()
 print(stdout)
